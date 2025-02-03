fa69afe574232b13a898e4ec6e40e3dd6ec10eb5
robot703 <robot703@naver.com>
2025-02-03 12:45:11 +0900

    [SWexpertAcademy-22039]: 피보나치수 분배

---
+++ SWexpertaccdemy/피보나치수분배_22039.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class 피보나치수분배_22039 {
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T= Integer.parseInt(br.readLine());
+
+        for(int t=0; t<T; t++){
+            int n = Integer.parseInt(br.readLine());
+            fivonachi(n);
+        }
+    }
+
+    static int fivonachi(int a){
+        return 
+    }
+}
